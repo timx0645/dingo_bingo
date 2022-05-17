986278ca81d3d505cd075977c3a58b7f4cf5026b
timx0645 <tim.s.h@hotmail.com>
2022-05-17 20:30:13 +0200

    Fixed height

--- components/header/header.style.js
+++ components/header/header.style.js
@@ -1,14 +1,20 @@
 import styled from 'styled-components'
 
 const HeaderWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 50px 50px;
-  div {
-    align-self: center;
-  }
+  display: flex; 
+  flex-wrap: wrap; 
+  padding: 0 50px; 
+  div {  
+    align-self: center; 
+    min-height: 30px; 
+    p.tal { 
+      text-align: center; 
+      width: 100%; 
+      min-height: 50px; 
+    } 
+  } 
   img {
-      width: 170px;
+      width: 150px;
       margin: -47px 0 0 0;
       position: absolute;
   }
